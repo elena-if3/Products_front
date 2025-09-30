08da7e12d9a24adf75eac72f533362d8289bc2c0
Elena Rottin <elena.rottin@gmail.com>
2025-09-30 21:46:03 +0200

    Edit product OK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Products } from './features/products/products';
 import { CreateNewProduct } from './features/products/create-new-product/create-new-product';
+import { EditProduct } from './features/products/edit-product/edit-product';
 
 export const routes: Routes = [
   {
@@ -21,4 +22,8 @@ export const routes: Routes = [
     path: 'products/new',
     component: CreateNewProduct,
   },
+  {
+    path: 'products/edit/:id',
+    component: EditProduct
+  }
 ];
--- src/app/features/products/edit-product/edit-product.ts
+++ src/app/features/products/edit-product/edit-product.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PageTitle } from "../../../shared/components/page-title/page-title";
+import { ProductForm } from "../components/product-form/product-form";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../form/interfaces/product.interface';
+import { ProductService } from '../services/product.service';
+import { handleError } from '../../../core/tools/errorHandler';
 
 @Component({
   selector: 'app-edit-product',
-  imports: [],
+  imports: [PageTitle, ProductForm],
   templateUrl: './edit-product.html',
   styleUrl: './edit-product.css'
 })
-export class EditProduct {
+export class EditProduct implements OnInit {
+  private activeRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private productId: number = 0;
+  public product?: Product;
+  public error: string = "";
 
-}
+  private readonly productService = inject(ProductService);
+
+  public async ngOnInit() {
+      this.productId = this.activeRoute.snapshot.params["id"];
+      this.product = await this.productService.getOneById(this.productId).catch(handleError);
+
+      if(!this.product) {
+        this.error = "Product not found"
+      }
+  }
+
+  public async update(product: Product){
+    try {
+      await this.productService.update(this.productId, product);
+      await this.router.navigate(["/products"]);
+    } catch (error) {
+      this.error = "Error while updating...";
+    };
+  };
+};
